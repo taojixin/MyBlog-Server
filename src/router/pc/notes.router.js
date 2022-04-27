a7233845bf30e8ec7de07cde0a631ce9841b4354
Tao <491675919@qq.com>
2022-04-27 23:11:11 +0800

    2022.4.27

--- src/router/pc/notes.router.js
+++ src/router/pc/notes.router.js
@@ -5,7 +5,15 @@ const { gitBasic } = require('../../notes/gitBasic')
 const { jqueryBasic } = require('../../notes/jqueryBasic')
 const { vueBasic } = require('../../notes/vueBasic')
 
-const {addNote} = require('../../controller/notes.controller')
+const {
+  addNote,
+  getSomeNote,
+  getNoteContent
+} = require('../../controller/notes.controller')
+// admin端的（懒得再写一遍写用一下）
+const {
+  getSortList
+} = require('../../controller/study.controller')
 
 notesRouter.post('/addnote', addNote)
 
@@ -13,5 +21,12 @@ notesRouter.get('/vuebasic', vueBasic)
 notesRouter.get('/cssbasic', cssBasic)
 notesRouter.get('/gitbasic', gitBasic)
 notesRouter.get('/jquerybasic', jqueryBasic)
+
+// 获取笔记分类接口
+notesRouter.get('/getsortlist', getSortList)
+// 根据分类查询笔记
+notesRouter.post('/getsomenote', getSomeNote)
+// 根据id获取笔记内容
+notesRouter.post('/getnotecontent', getNoteContent)
 
 module.exports = notesRouter;
--- src/service/notes.service.js
+++ src/service/notes.service.js
@@ -9,6 +9,18 @@ class NotesService {
     const result = connections.execute(statement, [mesarray])
     return result
   }
+  // 根据分类获取笔记
+  async getSomeNote(sort) {
+    const statement = `SELECT id,note_title,note_describe,note_createtime,note_sort FROM notes_test WHERE note_sort=?;`
+    const result = await connections.execute(statement, [sort])
+    return result[0]
+  }
+  // 根据id获取内容
+  async getNoteContent(id) {
+    const statement = `SELECT note_content FROM notes_test WHERE id=?;`
+    const result = await connections.execute(statement, [id+''])
+    return result[0][0]
+  }
 }
 
 module.exports = new NotesService();
